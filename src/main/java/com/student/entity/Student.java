cbb6dec1ca8a7d1edeb6d74c939c22a0d3ce70ff
Mohini-Singh-Jaiswal <104339986+Mohini-Singh-Jaiswal@users.noreply.github.com>
2022-04-27 16:07:17 +0530

    Update Student.java

--- src/main/java/com/student/entity/Student.java
+++ src/main/java/com/student/entity/Student.java
@@ -16,45 +16,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Student {
 
-	public Student() {
-		super();
-	}
-	public Student(Long studentId, String studentName, String studentCourse, String branch) {
-		super();
-		this.studentId = studentId;
-		this.studentName = studentName;
-		this.studentCourse = studentCourse;
-		this.branch = branch;
-	}
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long studentId;
 	String studentName;
 	String studentCourse;
 	String branch;
-	public Long getStudentId() {
-		return studentId;
-	}
-	public void setStudentId(Long studentId) {
-		this.studentId = studentId;
-	}
-	public String getStudentName() {
-		return studentName;
-	}
-	public void setStudentName(String studentName) {
-		this.studentName = studentName;
-	}
-	public String getStudentCourse() {
-		return studentCourse;
-	}
-	public void setStudentCourse(String studentCourse) {
-		this.studentCourse = studentCourse;
-	}
-	public String getBranch() {
-		return branch;
-	}
-	public void setBranch(String branch) {
-		this.branch = branch;
-	}
+	
 	
 }
